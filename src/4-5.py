b688602206b6813b7bc3f32f1c400d88b3aa3adc
Zepyhrus <sherkfung@gmail.com>
2021-01-08 16:36:08 +0800

    update 4-5

--- src/4-5.py
+++ src/4-5.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 tf.compat.v1.disable_v2_behavior()
 
-def load_sample(sample_dir):
+def load_sample(sample_dir, shuffle=True):
   print('loading sample dataset..')
   
   lfilenames = []
@@ -25,10 +25,45 @@ def load_sample(sample_dir):
 
   labels = [labdict[i] for i in labelsnames]
 
-  return shuffle((np.asarray(lfilenames), np.asarray(labels))), np.asarray(lab)
+  if shuffle:
+    return shuffle(
+      (np.asarray(lfilenames), np.asarray(labels))
+    ), np.asarray(lab)
+  else:
+    return (np.asarray(lfilenames), np.asarray(labels)), np.asarray(lab)
+
+
+def make_tfrec(filenames, labels, target):
+  writer = tf.io.TFRecordWriter(target)
+
+  for i in tqdm(range(len(labels))):
+    img = Image.open(filenames[i])
+    img = img.resize((28, 28))
+    img_raw = img.tobytes()
+    example = tf.train.Example(
+      features=tf.train.Features(
+        feature={
+          'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[labels[i]])),
+          'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+        }
+      )
+    )
+
+    writer.write(example.SerializeToString())
+  writer.close()
+
 
 if __name__ == "__main__":
   directory = 'data/mnist/'
+
+  (filenames, labels), _ = load_sample(directory, shuffle=False)
+
+  print(filenames[:2], labels[:2], _)
+
+  target = 'data/mnist.tfrecords'
+
+  make_tfrec(filenames, labels, target)
+
 
 
 
