bc7b9c3ab2a6c987365b8591e3193f0345eba2e1
Zepyhrus <sherkfung@gmail.com>
2021-01-08 10:38:32 +0800

    add 0-2

---
+++ src/0-2.py
@@ -0,0 +1,17 @@
+import tensorflow_datasets as tfds
+
+
+if __name__ == "__main__":
+  # print(tfds.list_builders())
+
+  # load datasets 
+  ds_train, ds_test = tfds.load(name='mnist', split=['train', 'test'])
+
+  ds_train = ds_train.shuffle(1000).batch(128).prefetch(10)
+
+  for features in ds_train.take(1):
+    image, label = features['image'], features['label']
+
+
+
+
