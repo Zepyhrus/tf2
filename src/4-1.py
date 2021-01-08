9ccf44d4536ef1daa007499bb74e3304bb413110
Zepyhrus <sherkfung@gmail.com>
2021-01-08 14:35:04 +0800

    add 4-2

--- src/4-1.py
+++ src/4-1.py
@@ -12,12 +12,11 @@ plt.rcParams['figure.autolayout'] = True
 tf.compat.v1.disable_v2_behavior()
 
 
-def generate_data(training_epochs, batch_size=100):
-  for i in range(training_epochs):
-    train_X = np.linspace(-batch_size/100, batch_size/100, batch_size)
-    train_Y = 2 * train_X + np.random.randn(*train_X.shape)
+def generate_data(batch_size=100):
+  train_X = np.linspace(-batch_size/100, batch_size/100, batch_size)
+  train_Y = 2 * train_X + np.random.randn(*train_X.shape)
 
-    yield shuffle(train_X, train_Y), i
+  yield train_X, train_Y
 
 
 if __name__ == "__main__":
@@ -26,14 +25,16 @@ if __name__ == "__main__":
 
   # 
   training_epochs = 20
+
   with tf.compat.v1.Session() as sess:
-    for (x, y), epoch in generate_data(training_epochs):
-      xv, yv = sess.run([X_input, Y_input], feed_dict={X_input: x, Y_input: y})
-      print(epoch, '| x.shape:', np.shape(xv), '| x[:3]: ', xv[:3])
-      print(epoch, '| y.shape:', np.shape(yv), '| y[:3]: ', yv[:3])
+    for epoch in range(training_epochs):
+      for x, y in generate_data():
+        xv, yv = sess.run([X_input, Y_input], feed_dict={X_input: x, Y_input: y})
+        print(epoch, '| x.shape:', np.shape(xv), '| x[:3]: ', xv[:3])
+        print(epoch, '| y.shape:', np.shape(yv), '| y[:3]: ', yv[:3])
 
-  train_data = list(generate_data(1))[0]
+  train_data = list(generate_data())[0]
 
-  plt.plot(train_data[0][0], train_data[0][1], 'ro', label='Original data')
+  plt.plot(train_data[0], train_data[1], 'ro', label='Original data')
   plt.legend()
   plt.show()
