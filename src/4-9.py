3c53eb4bb72b42a5838f81650db2aa0b9036069b
Zepyhrus <sherkfung@gmail.com>
2021-01-09 09:58:14 +0800

    update 4-9

--- src/4-9.py
+++ src/4-9.py
@@ -5,7 +5,16 @@ tf.compat.v1.disable_v2_behavior()
 
 
 if __name__ == "__main__":
-  
+  dataset1 = tf.data.Dataset.from_tensor_slices( list(range(5)) )
+  dataset1 = dataset1.shuffle(buffer_size=10).repeat()
+
+  one_element1 = get_one(dataset1)
+
+  with tf.compat.v1.Session() as sess:
+    for i in range(20):
+      print(sess.run(one_element1))
+
+
 
 
 
