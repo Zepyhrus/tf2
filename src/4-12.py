b78447c433809123cee7897f1a5d3ad2794e540d
Zepyhrus <sherkfung@gmail.com>
2021-01-09 22:41:22 +0800

    add 4-13

--- src/4-12.py
+++ src/4-12.py
@@ -44,12 +44,15 @@ def _norm_img(img, size, ch=1, flattenflag=False):
 
 def _rotated(img):
   shift_y, shift_x = np.array(img.shape[:2], np.float32) / 2.
+
   tf_rotate = transform.SimilarityTransform(rotation=np.deg2rad(random.randint(0, 30)))
   tf_shift = transform.SimilarityTransform(translation=[-shift_x, -shift_y])
   tf_shift_inv = transform.SimilarityTransform(translation=[shift_x, shift_y])
-  return transform.wrap(img, (tf_shift + (tf_rotate + tf_shift_inv)).inverse)
 
-def _rotated_wrap(img):
+  return transform.warp(img.numpy(), (tf_shift + (tf_rotate + tf_shift_inv)).inverse)
+
+
+def _rotate_wrap(img):
   img_rotated = tf.py_function( _rotated, [img], [tf.float32] )
 
   return img_rotated[0]
@@ -65,9 +68,44 @@ def _random_rotated(img, label):
 
   return img_decoded, label
 
+def dataset(directory, size, batch_size, random_rotated=False):
+  (filenames, labels), _ = load_sample(directory, shuffleflag=False)
+
+  def _parseone(filename, label):
+    """ Reading and handle image """
+    image_string = tf.io.read_file(filename)
+    image_decoded = tf.image.decode_image(image_string)
+    image_decoded.set_shape([None, None, None])
+    image_decoded = _distort_img(image_decoded, size)
+    image_decoded = tf.image.resize(image_decoded, size)
+    image_decoded = _norm_img(image_decoded, size)
+    image_decoded = tf.cast(image_decoded, dtype=tf.float32)
+    label = tf.cast( tf.reshape(label, []), dtype=tf.int32 )
+
+    return image_decoded, label
+
+  dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+  dataset = dataset.map(_parseone)
+
+  if random_rotated:
+    dataset = dataset.map(_random_rotated)
+  
+  dataset = dataset.batch(batch_size)
+
+  return dataset
+
 
 if __name__ == "__main__":
-  (filenames, labels), lab = load_sample('data/mnist/train', shuffleflag=True)
+  save_dir = 'data\\mnist'
+  (filenames, labels), lab = load_sample(save_dir, shuffleflag=True)
+  
+  size = [96, 96]
+  batch_size = 10
+
+  tdataset2 = dataset(save_dir, size, batch_size, True)
+
+  for step, value in enumerate(tdataset2):
+    show_img(step, value[1].numpy(), np.asarray( value[0] * 255, np.uint8 ), 10)
 
   
 
