807f93a5cc81dc424ef76be3103b68fb3d2dded9
Zepyhrus <sherkfung@gmail.com>
2021-01-15 14:25:38 +0800

    add book

--- book/2-6.py
+++ book/2-6.py
@@ -2,27 +2,36 @@ import tensorflow as tf
 import numpy as np
 
 if __name__ == "__main__":
-  input_xs = np.random.rand(1000)
+  input_xs = np.random.rand(1000).astype(np.float32)
   input_ys = 3 * input_xs + 0.217 + np.random.randn(*input_xs.shape) * 0.1
+  input_ys = input_ys.astype(np.float32)
 
   print(input_xs[:3], input_ys[:3])
 
+  # model
   weight = tf.Variable(1., dtype=tf.float32, name='weight')
   bias = tf.Variable(1., dtype=tf.float32, name='bias')
-
-  opt = tf.optimizers.Adam(1e-1)
 
   def model(xs):
     logits = tf.multiply(xs, weight) + bias
 
     return logits
-  
-  for xs, ys in zip(input_xs, input_ys):
-    xs = np.reshape(xs, [1])
-    ys = np.reshape(ys, [1])
 
+  # optimizer
+  opt = tf.optimizers.Adam(1e-1)
+
+  
+  # loss
+  loss = tf.losses.MeanSquaredError()
+  
+  # dataset
+  dataset = tf.data.Dataset.from_tensor_slices((input_xs, input_ys))
+  dataset = dataset.shuffle(buffer_size=1000).batch(256).repeat(20)
+
+  # training
+  for xs, ys in dataset:
     with tf.GradientTape() as tape:
-      _loss = tf.reduce_mean(tf.pow(model(xs) - ys, 2)) / (2 * 1000)
+      _loss = loss(model(xs), ys)
     
     grads = tape.gradient(_loss, [weight, bias])
     opt.apply_gradients(zip(grads, [weight, bias]))
